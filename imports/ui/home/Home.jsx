9053739defe03173bba374d86156321e2fa5c328
Srinivas Baride <srini@exidio.co>
2021-03-25 21:03:15 +0530

    Modified colors and titles

--- imports/ui/home/Home.jsx
+++ imports/ui/home/Home.jsx
@@ -16,8 +16,8 @@ export default class Home extends Component{
     render() {
         return <div id="home">
             <Helmet>
-                <title>The Big Dipper | Cosmos Explorer by Forbole</title>
-                <meta name="description" content="Cosmos is a decentralized network of independent parallel blockchains, each powered by BFT consensus algorithms like Tendermint consensus." />
+                <title>The Big Dipper | Sentinel Explorer by Forbole</title>
+                <meta name="description" content="Sentinel is a decentralized network of independent parallel blockchains, each powered by BFT consensus algorithms like Tendermint consensus." />
             </Helmet>
             <ChainInfo/>
             <Consensus />
